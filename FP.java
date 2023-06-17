bfe7940681e7d6f498fa6b80c6fa7beba87283cb
Sean Mayer <apart-things.0p@icloud.com>
2023-06-17 17:34:13 +0100

    Functional Programming - adding filters

--- FP.java
+++ FP.java
@@ -2,25 +2,51 @@ import java.util.List;
 
 public class FP {
     public static void main(String[] args) {
-        printAllNumbersInListStructure(List.of(12, 9, 13, 4, 6, 2, 4, 12, 15));
-        printAllNumbersInListFuntional(List.of(12, 9, 13, 4, 6, 2, 4, 12, 15));
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+        printAllNumbersInListStructure(numbers);
+        printAllNumbersInListFuntional(numbers);
+        printAllNumbersInListFuntional1(numbers);
+        printEvenNumbersInListStructure(numbers);
+        printEvenNumbersInListFunctional(numbers);
+    }
+
+    private static void printAllNumbersInListStructure(List<Integer> numbers) {
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+
+    private static void printAllNumbersInListFuntional(List<Integer> numbers) {
+        numbers.stream()
+                .forEach(FP::print); // Method reference
     }
 
     private static void print(int number) {
         System.out.println(number);
     }
 
-    private static void printAllNumbersInListFuntional(List<Integer> numbers) {
-
+    private static void printAllNumbersInListFuntional1(List<Integer> numbers) {
         numbers.stream()
-                .forEach(FP::print); // Method reference
+                .forEach(System.out::println); // Method reference
     }
 
-    private static void printAllNumbersInListStructure(List<Integer> numbers) {
-        // How to loop the numbers?
-        // How to print?
+    //Print only even numbers
+    private static void printEvenNumbersInListStructure(List<Integer> numbers) {
         for (int number : numbers) {
-            System.out.println(number);
+            if (number % 2 == 0) {
+                System.out.println(number);
+            }
         }
     }
+
+    private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+        numbers.stream()
+                .filter(FP::isEven) // Method reference
+                .forEach(System.out::println); // Method reference
+    }
+
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
 }
